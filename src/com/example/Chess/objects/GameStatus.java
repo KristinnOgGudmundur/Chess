1f7b64c8d97c83e7e1a987065537d2e56790bb18
gudmundurste12 <gudmundurste12@ru.is>
2014-10-17 14:20:06 +0000

    Added documentation to GameStatus and MoveOptions, and added CANCASTLE to MoveOptions

--- src/com/example/Chess/objects/GameStatus.java
+++ src/com/example/Chess/objects/GameStatus.java
@@ -4,11 +4,11 @@ package com.example.Chess.objects;
  * Created by Gvendur Stefáns on 8.10.2014.
  */
 public enum GameStatus {
-	TURN_PLAYER1,
-	TURN_PLAYER2,
-	VICTORY_PLAYER1,
-	VICTORY_PLAYER2,
-	STALEMATE,
-	CHECK_PLAYER1,
-	CHECK_PLAYER2
+	TURN_PLAYER1,		//It's player 1's turn
+	TURN_PLAYER2,		//It's player 2's turn
+	VICTORY_PLAYER1,	//Player 1 has won
+	VICTORY_PLAYER2,	//Player 2 has won
+	STALEMATE,			//The game has ended because the active player can't move
+	CHECK_PLAYER1,		//It's player 1's turn and his/her king is checked
+	CHECK_PLAYER2		//It's player 2's turn and his/her king is checked
 }
--- src/com/example/Chess/objects/MoveStatus.java
+++ src/com/example/Chess/objects/MoveStatus.java
@@ -4,7 +4,8 @@ package com.example.Chess.objects;
  * Created by Gvendur Stefáns on 8.10.2014.
  */
 public enum MoveStatus {
-	CANMOVE,
-	CANKILL,
-	PROTECTS
+	CANMOVE,	//The piece can move to the position
+	CANKILL,	//The piece can kill an enemy piece at the position
+	PROTECTS,	//The piece protects the friendly piece at the position
+	CANCASTLE	//The piece is a King and can castle to the position
 }
