fe1aa449181d3f348f5f31dd4a283e9f56aa5969
gudmundurste12 <gudmundurste12@ru.is>
2014-10-20 14:25:27 +0000

    Added Coordinate.isInRange()

--- src/com/example/Chess/objects/Coordinate.java
+++ src/com/example/Chess/objects/Coordinate.java
@@ -21,6 +21,11 @@ public class Coordinate {
 	}
 
 
+	public boolean isInRange(int lower, int upper){
+		return (col >= lower) && (row >= lower) && (col <= upper) && (col <= upper);
+	}
+
+
 	@Override
 	public boolean equals(Object other){
 		if(other.getClass() != Coordinate.class){
